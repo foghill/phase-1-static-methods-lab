f921ae4325082384a323b34eb53b187a8d5eb918
foghill <ben@thefoghill.com>
2022-04-06 12:02:59 -0400

    commit tree 03969dc6000e10379e6517c7b86dbaa6c47c7f25

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+static sanitize(str) {
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' );
+    
+}
+
+static titleize(str) {
+  // not sure how to do this one
+}
+
 }
